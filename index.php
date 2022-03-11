2329a76fea1220bf54a1707f9190b0be4b0e9a58
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-11 18:06:31 +0100

    Header completato

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Policy</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <header>
+        <div class="header-top">
+            <div class="left">
+                <img class="logo" src="./img/Google-logo.png" alt="logo google">
+                <span>Privacy e termini</span>
+            </div>
+
+            <div class="right">
+                <i class="fas fa-th"></i>
+                <i class="fas fa-user-circle"></i>
+            </div>
+            
+        </div>
+        <div class="header-bottom">
+            <ul>
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li class="active">Domande frequenti</li>
+            </ul>
+        </div>
+
+    </header>
+
+    <main>
+
+    </main>
+    
+</body>
+</html>
